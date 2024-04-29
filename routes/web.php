87acfe1dd1aeb5328485152e0494ef28f7ceed53
andrebuenafe <andre.buenafe1437@gmail.com>
2024-04-29 17:10:49 +0800

    second commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('register',[RegisterController::class,'register'])->name('register');
