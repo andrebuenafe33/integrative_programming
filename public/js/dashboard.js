677a0b13c83fcaa94150e4561151bb0a358b6273
andrebuenafe <andre.buenafe1437@gmail.com>
2024-05-30 19:22:17 +0800

    akong gi butangan ug boostrap modal and delete para choy

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,32 +1,32 @@
-// const hamBurger = document.querySelector(".toggle-btn");
+const hamBurger = document.querySelector(".toggle-btn");
 
-// hamBurger.addEventListener("click", function () {
-//     document.querySelector("#sidebar").classList.toggle("expand");
-// });
-
-// document
-//     .getElementById("navbarDropdown")
-//     .addEventListener("click", function (event) {
-//         event.preventDefault();
-//         document.getElementById("dropdownToggle").classList.toggle("show");
-//     });
-
-$(document).ready(function () {
-    $("#logout-form").submit(function (event) {
-        event.preventDefault();
-
-        $.ajax({
-            url: $(this).attr("action"),
-            method: $(this).attr("method"),
-            data: $(this).serialize(),
-            success: function (response) {
-                window.location.href = "/";
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-                console.error(status.responseText);
-                console.error(error.responseText);
-            },
-        });
-    });
+hamBurger.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+document
+    .getElementById("navbarDropdown")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        document.getElementById("dropdownToggle").classList.toggle("show");
+    });
+
+// $(document).ready(function () {
+//     $("#logout-form").submit(function (event) {
+//         event.preventDefault();
+
+//         $.ajax({
+//             url: $(this).attr("action"),
+//             method: $(this).attr("method"),
+//             data: $(this).serialize(),
+//             success: function (response) {
+//                 window.location.href = "/";
+//             },
+//             error: function (xhr, status, error) {
+//                 console.error(xhr.responseText);
+//                 console.error(status.responseText);
+//                 console.error(error.responseText);
+//             },
+//         });
+//     });
+// });
